f17db9614db92a0eb24f9b6f5570a549ae99e071
Wesley Morgan <wmorga20@gmail.com>
2020-09-15 10:33:44 -0500

    right arrow triggers movements

--- javascript/A_storyroom.js
+++ javascript/A_storyroom.js
@@ -4,5 +4,11 @@ document.onkeydown = function(event) {
 	switch (event.keyCode) {
 		case 40:
 			window.location.replace("B_storyroom.html");
+	break;
+		case 39:
+		  document.getElementById("letterA").classList.add("active");
 	}
 }
+
+
+gi
